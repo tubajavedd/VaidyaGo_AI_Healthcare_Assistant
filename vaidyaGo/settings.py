4b9dd4e44be4e379687671a6bba67e214099b6da
Tuba Javed <javedtuba1@example.com>
2026-04-09 14:37:27 +0000

    do changes in AdminLogin

--- vaidyaGo/settings.py
+++ vaidyaGo/settings.py
@@ -226,4 +226,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "javedtuba1@gmail.com"
-EMAIL_HOST_PASSWORD = "Tub@javed0825"
+EMAIL_HOST_PASSWORD = "Tub@javed0825julyy"
+
+
+
+
